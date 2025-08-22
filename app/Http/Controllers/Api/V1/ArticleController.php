4124fa77adc5eb46a5f9ea39fb1f8f5ad5f5efd2
Dian Rusdianto <dianrusdianto06@gmail.com>
2025-08-22 13:53:01 +0700

    feat: add recommended article on detail article

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -10,8 +10,18 @@ class ArticleController extends Controller
 {
     public function show($slug)
     {
-        // buatkan detail artikel menggunakan slug
+        // Ambil artikel berdasarkan slug
         $article = Article::where('slug', $slug)->firstOrFail();
-        return $this->getSuccessResponse($article);
+
+        // Ambil 6 artikel lain secara acak, kecuali artikel yang sedang ditampilkan
+        $articles = Article::where('id', '!=', $article->id)
+            ->inRandomOrder()
+            ->limit(6)
+            ->get();
+
+        return $this->getSuccessResponse([
+            'article' => $article,
+            'articles' => $articles,
+        ]);
     }
 }
