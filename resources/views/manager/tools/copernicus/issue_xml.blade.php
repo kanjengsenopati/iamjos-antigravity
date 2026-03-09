a21fa01d95a21c5d01e8d5cb909b6a33fc3c48da
Dian Rusdianto <dianrusdianto06@gmail.com>
2026-03-09 22:16:31 +0700

    feat: Add Blade templates for generating Copernicus article and issue XML exports.

--- resources/views/manager/tools/copernicus/issue_xml.blade.php
+++ resources/views/manager/tools/copernicus/issue_xml.blade.php
@@ -1,5 +1,5 @@
 <ici-import>
-    <journal issn="{{ $journal->issn }}" />
+    <journal issn="{{ $journal->issn_online }}" />
     @foreach($issues as $issue)
         <issue number="{{ $issue->number }}" volume="{{ $issue->volume }}" year="{{ $issue->year }}" />
         @foreach($issue->submissions as $article)
@@ -7,17 +7,19 @@
                 <type>Original article</type>
                 <languageVersion language="{{ $article->language ?? 'en' }}">
                     <title><![CDATA[{{ $article->title }}]]></title>
-                    <abstract><![CDATA[{{ trim(html_entity_decode(strip_tags(str_replace('&nbsp;', ' ', $article->abstract)), ENT_QUOTES, 'UTF-8')) }}]]></abstract>
+                    <abstract><![CDATA[{{ trim(strip_tags(html_entity_decode(str_replace('&nbsp;', ' ', $article->abstract), ENT_QUOTES, 'UTF-8'))) }}]]></abstract>
                     <pdfFileUrl><![CDATA[{{ route('journal.article.download.pdf', ['journal' => $journal->slug, 'seq_id' => $article->seq_id, 'filename' => \Str::slug($article->title)]) }}]]></pdfFileUrl>
-                    <publicationDate>{{ $article->published_at ? $article->published_at->format('Y-m-d') : '' }}</publicationDate>
+                    <publicationDate>{{ $article->published_at ? $article->published_at->format('Y-m-d') : ($issue->published_at ? $issue->published_at->format('Y-m-d') : '') }}</publicationDate>
                 </languageVersion>
                 <authors>
                     @foreach($article->authors as $index => $author)
                         <author>
                             @php
-                                $nameParts = explode(' ', trim($author->name));
+                                $fullName = trim(($author->given_name ?? '') . ' ' . ($author->family_name ?? ''));
+                                if (empty($fullName)) $fullName = $author->name ?? '';
+                                $nameParts = explode(' ', trim($fullName));
                                 $surname = count($nameParts) > 1 ? array_pop($nameParts) : '';
-                                $name = count($nameParts) > 0 ? implode(' ', $nameParts) : $author->name;
+                                $name = count($nameParts) > 0 ? implode(' ', $nameParts) : $fullName;
                             @endphp
                             <name><![CDATA[{{ $name }}]]></name>
                             <surname><![CDATA[{{ $surname }}]]></surname>
