b61eca76110aa7301e79f3c0f859e2bd3efe70d1
IamJOS Developer <developer@iamjos.id>
2026-06-26 16:57:20 +0700

    fix: support journal and journalSlug parameters in AppLayout component to prevent preview page crash

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -8,10 +8,14 @@ use Illuminate\View\View;
 class AppLayout extends Component
 {
     public $title;
+    public $journal;
+    public $journalSlug;
 
-    public function __construct($title = null)
+    public function __construct($title = null, $journal = null, $journalSlug = null)
     {
         $this->title = $title;
+        $this->journal = $journal;
+        $this->journalSlug = $journalSlug;
     }
 
     public function render(): View
