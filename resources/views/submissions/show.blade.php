b40b68d7cecf9b6ad2e8a9c83acdc14521cea962
IamJOS Developer <developer@iamjos.id>
2026-07-15 08:25:47 +0700

    fix: repair broken HTML tags in Reorder Modal causing all modals to be invisible

--- resources/views/submissions/show.blade.php
+++ resources/views/submissions/show.blade.php
@@ -3845,7 +3845,9 @@ $selectedRound = $allRounds->firstWhere('round', $selectedRoundNumber) ?? $curre
                         x-transition:leave="ease-in duration-200"
                         x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                         x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        class="relative z-50 inline-block align-bottom bg-white rounded-xl px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl                        <div class="sm:flex sm:items-start mb-5">
+                        class="relative z-50 inline-block align-bottom bg-white rounded-xl px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+
+                        <div class="sm:flex sm:items-start mb-5">
                             <div
                                 class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10">
                                 <i class="fa-solid fa-arrow-down-short-wide text-indigo-600"></i>
@@ -3891,6 +3893,12 @@ $selectedRound = $allRounds->firstWhere('round', $selectedRoundNumber) ?? $curre
                                 {{ $isId ? 'Batal' : 'Cancel' }}
                             </button>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
             {{-- NEW DISCUSSION MODAL --}}
         <div x-show="discussionModalOpen" style="display: none;" class="fixed inset-0 z-50 overflow-y-auto"
             aria-labelledby="modal-title" role="dialog" aria-modal="true">
